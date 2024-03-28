78a19b7f32cbbfd01e198046415f355741cd4e1f
zuka <157312286+zukadzm@users.noreply.github.com>
2024-03-28 09:21:37 -0700

    Add files via upload

---
+++ day10/lesson10.2.py
@@ -0,0 +1,6 @@
+def average_arithmetic(number_list):
+    all = sum(number_list)
+    result = all // len(number_list)
+    print(result)
+
+average_arithmetic([1,2,3])
---
+++ day10/lesson10.py
@@ -0,0 +1,4 @@
+def name_(name):
+     print("ზუკა,ქორიძე"[0::5].split(","))
+
+name_(1)
