589812a3d3ad82765ffa727585d675f9da08b75f
zukadzm <zuraqoridze988@gmail.com>
2024-06-03 00:11:22 -0700

    commit

---
+++ day30/classwork/classwork30.py
@@ -0,0 +1,14 @@
+# შექმენით თქვენს შესახებ დიქშენერი მიწვდთ ყველა აითემს for loop დახმარებით და დააჯოინეთ ერთად თქვენი მნიშვნელობები / value
+
+person = {
+    "name": "Zuka",
+    "surname": "Qoridze",
+    "age": 14,
+}
+
+
+x = person.values()
+c = person.keys()
+
+print(x,c)
+
---
+++ day30/homework/lesson30.py
@@ -0,0 +1,16 @@
+family = {
+    "Myname":"Zuka",
+    "Mom":"Lela",
+    "Dad":"Gia",
+    "sister":"Lika",
+    "Brother":"Gigi"
+}
+
+
+joinlstn = " ".join(family)    
+print(joinlstn)
+
+x = family.values()
+c = family.keys()
+
+print(x,c,joinlstn)
