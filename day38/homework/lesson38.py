f3a5d158691659794f3d70330f1f2c1563545237
zukadzm <zuraqoridze988@gmail.com>
2024-07-05 01:03:34 -0700

    commit

---
+++ day38/homework/lesson38.py
@@ -0,0 +1,123 @@
+# 120 - homework:
+
+#Python Syntax
+
+
+# Write a Python script that prints "Hello, World!"
+# 1) print("Hello, World")
+
+
+# Create a script that takes user input and prints it.
+
+# 2) name = input("Enter your name: ")
+# print(name)
+
+#Write a script that uses both single-line and multi-line comments.
+# 3) print("zuka")
+#print("qoridze")
+
+
+# Write a script that demonstrates indentation in Python.
+# 4) print("zuka")
+
+
+# Write a script that shows how to break lines in Python.
+# 5) print("If u need to breake lines, u can use , break func or just click delete and its break 1 line.")
+
+
+#Python Comments
+
+# Add comments to explain each line of a given script.
+# 6) each line, u writing codes in each line.
+
+
+#Write a script and use comments to explain a function's purpose.
+# 7) print("Lets, explain whats def means in python, def u can create your own function like, find() and everything you want.")
+
+
+#Add a block comment to describe the script's overall functionality.
+# 8)print("Lets, explain whats def means in python, def u can create your own function like, find() and everything you want.")
+# print("Lets, explain whats def means in python, def u can create your own function like, find() and everything you want.")
+
+
+#Use comments to disable a part of the script and re-enable it.
+# 9) print("Zuka") now script is turn off.
+
+
+#Write a script with intentional errors and comment on why they occur.
+# 10) num = zuka. this is error code.
+
+
+
+#Python Variables
+
+
+#Create and initialize multiple variables of different data types
+# 11) num = input("Enter your name: ")
+# print("Hello, World")
+
+
+#Swap the values of two variables.
+# 12) num1 = 12 23
+#     num2 = 23 12
+
+
+#Create a script that takes user input to assign values to variables.
+# 13)name = input("Enter your name: ")
+# print(name)
+
+
+#Write a script that uses both global and local variables.
+# 14)name = "zuka"
+# num = 14
+
+
+#Demonstrate variable naming conventions in Python.
+# 15)name = "zuka"
+# age = 14
+# live = "Georgia"
+
+
+
+#Python Data Types
+
+
+#create variables of types: integer, float, string, and boolean.
+# 16)integer = 14
+# floatt = 1.5
+# string = "zuka"
+# print(10 < 10)
+
+
+#Write a script to convert between different data types.
+# 17)string = "Hello ,World"
+# intt = 15
+# floatt = 4.3
+# print(string,intt,floatt)
+
+
+#Demonstrate the use of type() function to check variable types.
+# 18)name = "zuka"
+# age = 15
+# print(type(name))
+# print(type(age))
+
+
+
+#Perform basic arithmetic operations on different data types.
+# 19)num1 = 340
+# num2 = 12397
+# bowl = num1 + num2
+# print(bowl)
+
+
+
+#Write a script to compare different data types using relational operators.
+# 20)name = 'zuka'
+# print(type(name))
+# age = 15
+# print(type(age))
+
+
+
+#Python Numbers
