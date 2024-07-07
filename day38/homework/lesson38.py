e5ae45bedc9a0bfe5f0d589c67995b86efbd78a1
zukadzm <zuraqoridze988@gmail.com>
2024-07-07 08:03:55 -0700

    commit

--- day38/homework/lesson38.py
+++ day38/homework/lesson38.py
@@ -121,3 +121,837 @@
 
 
 #Python Numbers
+
+
+#Write a script to perform arithmetic operations.
+# 21) print(1 + 10)
+# print(10 / 2)
+# print(10 * 2)
+# print(130 % 2)
+
+
+
+#Create a script that generates a random number.
+
+# 22) num = 1,2,3,4,5
+
+# print(num [1])
+
+
+
+#Write a script that calculates the square root of a number.
+# 23) square_roof = 120
+# print(square_roof * 2)
+
+
+
+#Demonstrate the use of math functions like ceil() and floor().
+# 24) num = 1.5
+# num2 = 123
+# bowl = num2 / num
+# print(bowl)
+
+
+
+#Write a script to find the greatest common divisor (GCD) of two numbers.
+# 25) num1 = 13
+# num2 = 15
+# if num2 < num1:
+#     print(num2,"15 is more than 13")
+
+
+
+#Python Casting
+
+# Write a script to convert integers to floats and vice versa.
+# 26) floatt = 4.5
+# integer = 14
+# print(integer,floatt)
+
+
+
+#Create a script to convert strings to integers and floats.
+# 27)num1 = "12"
+# num2 = "43.2"
+# integer = int(num1)
+# floatt = float(num2)
+# print(integer,floatt)
+
+
+
+
+#Demonstrate casting between lists and tuples.
+# 28)num1 = "Hello"
+# print(num1)
+
+
+
+#Write a script to handle casting errors.
+# 29)num1 = hello
+# print(num1)
+
+
+
+#Create a script to convert a string representing a number to an integer.
+# 30)flt = 12.5
+# flt1 = float(flt)
+# print(flt1)
+
+
+
+
+#Python Booleans
+# Write a script to demonstrate the use of boolean values.
+# 31)print(20 < 10)
+
+
+
+#Create a script to perform logical operations (and, or, not).
+# 32)print(10 and 20)
+# print(10 or 40)
+# print(10,not 19)
+
+
+
+#Demonstrate the use of comparison operators to return boolean values.
+# 33)num1 = 10 < 20
+# return num1
+
+
+
+#Write a script that uses if-else statements based on boolean values.
+# 34)num1 = 124
+# num2 = 12
+
+# if num1 > num2:
+#     print(num1)
+
+
+
+
+#Create a script that returns a boolean result from a function.
+# 35)num1 = 12
+# num2 = 10
+# if num1 > num2:
+#     return num2
+
+
+
+
+#Python Operators
+#Write a script that demonstrates arithmetic operators.
+# 36)num1 = 10 > 20
+# num2 = 10 * 3
+# num3 = 213 / 2
+# print(num1,num2,num3)
+
+
+
+#Create a script to use assignment operators.
+# 37)a = 15
+# b = 20
+# bowl = a+b
+# print(bowl)
+
+
+
+#Write a script to show the use of comparison operators.
+# 38)print(10 < 20)
+
+
+
+#Demonstrate the use of logical operators in a script.
+# 39)print(20 < 30)
+
+
+
+#Create a script to use identity operators (is, is not).
+# 40)a = 10
+# b = 15
+# if a != b:
+#     print(a not b)
+
+
+
+#Python Lists
+# Write a script to create and print a list.
+# 41)lisn = ["Zuka" ,"Lika" ,"Gigi"]
+# print(lisn)
+
+
+
+#Create a script to add and remove elements from a list.
+# 42) listn = ["Apple" ,"Watermalon" ,"Banana" ,"Strawberry"]
+# listn.pop(3)
+# print(listn)
+# listn.append(2 ,"Hello")
+
+
+
+#Write a script to sort a list.
+# 43)listn = ["second" ,"first"]
+# listn.pop(0)
+# listn.append("Second")
+# print(listn)
+
+
+
+#Demonstrate the use of list comprehension.
+# 44)listn = ["Zuka" ,"Lika" ,"Gigi" ,"Lela" ,"Gia"]
+# print(listn)
+
+
+
+#Create a script to find the length of a list.
+# 45)listn = ["Zuka" ,"Lika" ,"Gigi" ,"Lela" ,"Gia"]
+# print(len(listn))
+
+
+
+#Python If...Else
+#Write a script that uses an if statement to check a condition.
+
+# 46) number = int(input("Guess number 1 to 50: "))
+# if number == 12:
+#     print("Correct")
+# else:
+#     print("Try again")
+#     exit()
+
+
+
+#Create a script that uses an if-else statement.
+# 47)number = int(input("Guess number 1 to 50: "))
+
+# if number != 12:
+#     print("Correct")
+# else:
+#     print("Try again")
+#     exit()
+
+
+
+#Write a script that uses an if-elif-else statement.
+# 48)print("Register")
+# name = input("Enter your name: ")
+# password = int(input("Create password: "))
+# repeat_password = int(input("Repeat your password: "))
+
+# if password == repeat_password:
+#     print("Register sucsesful")
+# elif print("HMmm try again"):
+
+
+#Demonstrate nested if statements.
+# 49)gues = int(input("Guess num 1 to 5: "))
+# if gues != 3:
+#     print("Wrong")
+
+
+
+#Write a script that uses the ternary operator.
+# 50)gues = int(input("Guess num 1 to 5: "))
+# if gues != 3:
+#     print("Wrong")
+# elif print("Correct: "):
+# else:
+#     print("Try again")
+
+
+#Python While Loops
+#Write a script to demonstrate a while loop.
+# 51)while 10 < 20:
+
+
+#Create a script that uses a while loop with a break statement.
+# 52)while 10 < 20:
+#     print(20)
+
+
+
+#Write a script that uses a while loop with a continue statement.
+# 53)while 20 < 10:
+#     print(True or False)
+
+
+
+#Demonstrate an infinite loop and how to stop it.
+# 54)while 10 < 20:
+
+
+
+#Create a script that uses a while loop to iterate over a list.
+# 55)listn = [1]
+# listn2 = [4]
+
+# while listn < listn2:
+#     print(listn2,"Is more than 1")
+
+
+
+#Python For Loops
+# Write a script to demonstrate a for loop.
+# 56)name = "Zuka"
+# for i in name:
+#    print(name)
+
+
+
+# Create a script that uses a for loop to iterate over a list.
+# 57)listn = ["Zuka" ,"Lika" ,"Gigi" ,"Lela" ,"Gia"]
+# for i in listn:
+#     print(listn)
+
+
+
+#Write a script that uses a for loop with the range() function.
+# 58)for i in range(10):
+#     print("Zuka")
+
+
+
+#Demonstrate nested for loops.
+# 59)guess = int(input("Guess number 10 to 50: "))
+
+# for i in guess:
+#     if guess != 32:
+#         print("Wrong")
+#     else:
+#         print("Correct")
+
+
+
+#Create a script that uses a for loop with an else clause.
+
+# 60)guess = int(input("Guess number 10 to 50: "))
+
+# for i in guess:
+#     if guess != 32:
+#         print("Wrong")
+#elif print("Correct")
+#         
+
+
+
+#Python Functions
+
+
+# Write a script to define and call a function.
+# 61)def name(namee):
+#     print(namee,"Hello")
+# name(12)
+
+
+# Create a script that uses a function with parameters.
+# 62)def name(age):
+#     print(age,14)
+# name("My age is,")
+
+
+
+#Write a script that uses a function with a return value.
+# 63)def name(age):
+#     print("Hello")
+#     return age
+# name("Hi")
+
+
+#Demonstrate the use of default parameters in a function.
+# 64)def name(age):
+#     print(age, "my age is")
+#     return age
+# name("15")
+
+
+
+#Create a script that uses a function with keyword arguments.
+# 65)def hello(light):
+#     print(light,"Green light")
+#     return hello
+# hello("1")
+
+
+
+
+#Python Operator
+#Write a program that uses all basic arithmetic operators (+, -, *, /, %, //, **) with two numbers.
+
+# 66)num1 = 10
+# num2 = 2
+# bowl = num1 + num2
+# bowl2 = num1 - num2
+# bowl3 = num1 * num2
+# bowl4 = num1 / num2
+# bowl5 = num1 % num2
+# bowl6 = num1 // num2
+# bowl7 = num1 ** num2
+# print(bowl,bowl2,bowl3,bowl4,bowl5,bowl6,bowl7)
+
+
+
+#Create a script to demonstrate the use of assignment operators (=, +=, -=, *=, /=, %=, //=, **=).
+
+# 67)num1 = 10
+# num2 = 2
+# bowl = num1 = num2
+# bowl2 = num1 += num2
+# bowl3 = num1 -= num2
+# bowl4 = num1 *= num2
+# bowl5 = num1 /= num2
+# bowl6 = num1 %= num2
+# bowl7 = num1 //= num2
+# bowl8 = num1 **= num2
+# print(bowl,bowl2,bowl3,bowl4,bowl5,bowl6,bowl7,bowl8)
+
+# Write a program to compare two numbers using comparison operators (==, !=, >, <, >=, <=).
+
+# 68)num1 = 10
+# num2 = 2
+# bowl = num1 = num2
+# bowl2 = num1 == num2
+# bowl3 = num1 != num2
+# bowl4 = num1 > num2
+# bowl5 = num1 < num2
+# bowl6 = num1 >= num2
+# bowl7 = num1 <= num2
+# print(bowl,bowl2,bowl3,bowl4,bowl5,bowl6,bowl7)
+
+
+
+#Demonstrate the use of logical operators (and, or, not) in a simple condition.
+# 69)guess = int(input("Guess number 1 to 50: "))
+
+# if guess == 10 or 12 and not 10 or 12:
+#     print("Succsesful")
+# else:
+#     print("Wrong")
+
+
+
+#Create a script that shows the use of bitwise operators (&, |, ^, ~, <<, >>).
+
+# 70)num1 = 10
+# num2 = 2
+# bowl = num1 = num2
+# bowl2 = num1 % num2
+# bowl3 = num1 | num2
+# bowl4 = num1 ^ num2
+# bowl5 = num1 ~ num2
+# bowl6 = num1 << num2
+# bowl7 = num1 >> num2
+# print(bowl,bowl2,bowl3,bowl4,bowl5,bowl6,bowl7)
+
+
+#Write a program that checks if a number is within a specified range using comparison and logical operators.
+# 71)def name(low,high):
+#     for i in range:
+#         if high < low:
+#             return high
+#         print("Done")
+
+
+#Use identity operators (is, is not) to compare objects and variables.
+# 72)num1 = 12
+# name = "zuka"
+
+# if num1 is not == name:
+#     return name
+
+
+
+
+#Write a program that uses membership operators (in, not in) to check if a value exists in a list.
+
+# 73)listn = ["Zuka" ,"Lika" ,"Gigi" ,"Gia" ,"Lela"]
+
+# for i in (len(listn)):
+#     if listn is not 6:
+#         print("Wrong")
+
+
+
+# Demonstrate the use of operator precedence by creating a complex expression.
+# 74)def find(char):
+#     result = []
+#     for i in range(len(char)):
+#         char += char
+#     return find,result
+# listn = ["Zuka" ,"Lika" ,"Gigi"]
+# find(listn, 2)
+
+
+
+#Write a program that takes two user inputs and performs all the basic arithmetic operations on them.
+# 75)number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter your second number: "))
+
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 * number2)
+# print(number1 / number2)
+# print(number1 ^ number2)
+# print(number1 // number2)
+# print(number1 | number2)
+
+
+
+# 76)Create a script that swaps the values of two variables without using a third variable.
+
+# num1 = 15
+# num2 = 124
+
+# for i in num1,num2:
+#     num2 == 15
+#     num1 == 124
+# print(num1,num2)
+
+
+
+#Python Lists
+#Create a list of five numbers and print the list.
+
+# 77)listn = [1 , 2 , 3 , 4 , 5]
+# print(listn)
+
+
+
+#Write a program to access elements from a list using positive and negative indexing.
+# 78)listn = [0 , 2 , 4 , 6 , 8 , 10]
+
+
+
+#Create a script to add, remove, and update elements in a list.
+# 79)listn = ["Apple" ,"penaple" ,"milk" ,"Strawberry"]
+# listn.append("alucha")
+# listn.pop(2)
+# print(listn)
+
+
+#Write a program that concatenates two lists.
+# 80)listn = ["Apple" ,"penaple" ,"milk" ,"Strawberry"]
+# listn1 = ["Zuka" ,"Lika" ,"Gigi"]
+# print(listn+listn1)
+
+
+
+#Create a script that finds the length of a list using the len() function.
+# 81)listn = ["Apple" ,"penaple" ,"milk" ,"Strawberry"]
+# print(len(listn))
+
+
+
+#rite a program that checks if a specified element is present in a list.
+
+# 82)listn = ["Apple" ,"penaple" ,"milk" ,"Strawberry"]
+
+# if listn == list:
+#     print("Yes")
+# else:
+#     print("No")
+
+
+#Create a script to slice a list and print the sliced list.
+# 83)listn = ["Apple" ,"penaple" ,"milk" ,"Strawberry"]
+
+# for i in listn:
+#     print(listn[0:2])
+#     print(listn[2:3])
+
+
+#Write a program that sorts a list in ascending and descending order.
+
+# 84)listn = [10,9,8,7,6,5,4,3,2,1]
+# print(listn)'
+
+
+#Create a script that demonstrates the use of list comprehension.
+# 85)listn = [10,9,8,7,6,5,4,3,2,1]
+# print(len(listn))
+
+
+#Write a program to find the maximum and minimum values in a list.
+# 86)listn = [10,9,8,7,6,5,4,3,2,1]
+# print(listn,min,max)
+
+
+
+#Create a script that reverses the elements of a list.
+
+# 87)listn = ["Apple" ,"penaple" ,"milk" ,"Strawberry"]
+# return listn
+
+
+
+
+#Python While Loops
+
+
+
+#Write a program that prints numbers from 1 to 10 using a while loop.
+
+# 88)while range(1,10):
+#     print(1,10)
+
+
+#Create a script that calculates the sum of the first 10 natural numbers using a while loop.
+
+# 89)while 10 < 1:
+#         return 1 < 10
+
+
+#Write a program that prints the multiplication table of a given number using a while loop.
+# 90)while 10 < 1:
+#        return 1 < 10
+
+
+#Create a script that finds the factorial of a number using a while loop.
+
+# 91)while 10 < 20:
+#     print(20)
+
+
+
+#Write a program that prints all even numbers between 1 and 50 using a while loop.
+
+# 92)while range(1,10):
+
+
+#Create a script that reverses a given number using a while loop.
+
+# 93)while 1 > 4:
+#     print(False)
+
+
+#Write a program that calculates the sum of digits of a number using a while loop.
+# 94)while 1 > 4:
+#      print(False)
+
+
+#Create a script that finds the greatest common divisor (GCD) of two numbers using a while loop.
+# 95)while True < False:
+
+
+#Write a program that prints the Fibonacci sequence up to a specified number using a while loop.
+# 96)while range(10 < 5):
+
+
+#Create a script that prints numbers from 1 to 100, but skips numbers divisible by 5, using a while loop.
+# 97)while 100 < 5:
+
+
+
+#Python For Loops 
+#Write a program that prints each element of a list using a for loop
+
+# 98)listn = ["Apple" ,"Banana" ,"Alucha"]
+
+# for i in listn:
+#     print(listn,[0],[1],[2])
+
+
+#Create a script that prints numbers from 1 to 10 using a for loop.
+
+# 99)for i in range(1, 10):
+#     print(i)
+
+
+
+#Write a program that calculates the sum of elements in a list using a for loop.
+
+# 100)listn = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10]
+# for i in listn:
+#     sum(listn)
+#     print(i)
+
+
+# Create a script that prints the multiplication table of a given number using a for loop.
+
+# 101)for i in range(10):
+#     print(i,*2)
+
+
+
+
+# Write a program that prints all even numbers between 1 and 50 using a for loop.
+
+# 102)for i in range(1, 50):
+#     print(i)
+
+
+
+# 103)Create a script that prints the Fibonacci sequence up to a specified number using a for loop.
+# for i in range(2,19):
+#     print(i)
+
+
+
+
+# Write a program that iterates over a dictionary and prints each key-value pair.
+
+# 104)name = {
+#     "name","Zuka",
+#     "age",15
+# }
+
+# print(name)
+
+
+
+# Create a script that finds the maximum and minimum values in a list using a for loop.
+
+# 105)lisnt = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+
+# new = lisnt.min()
+# new2 = lisnt.max()
+# print(new,new2)
+
+
+
+
+
+# Write a program that prints each character of a string using a for loop.
+
+# 106)listn = ["Apple" ,"penaple" ,"milk" ,"Strawberry"]
+
+# for i in listn:
+#     print(listn)
+
+
+
+
+# Create a script that prints the reverse of a list using a for loop.
+
+# 107)listn = ["Apple" ,"penaple" ,"milk" ,"Strawberry"]
+# for i in listn:
+#     print(listn)
+
+
+
+
+
+# Write a program that uses nested for loops to print a pattern, such as a triangle of stars.
+
+# 108)listn = [10 , 20 , 30]
+# print(listn)
+
+
+
+
+# Python Functions
+
+# Write a function that takes two numbers as arguments and returns their sum.
+
+# 109)def num(num2):
+#     result = sum
+#     for i in num2:
+#         if num == num2:
+#             return result
+#         print(num2)
+
+# Create a function that checks if a given number is prime.
+
+# 110)def char(bar):
+#     result = []
+#     for i in range(len(bar)):
+#         return result
+
+# Write a function that calculates the factorial of a number.
+# 111)print("i calculate your number")
+# num1 = int(input("Enter your first number: "))
+# num2 = int(input("Enter your second number: "))
+
+# def func(new):
+#     result = num1,num2
+#     for i in range(10(len(new))):
+#         num1 + num2 / num1 // num2 % num1
+#         return result
+#     print(func)
+
+
+
+# Create a function that returns the largest number in a list.
+
+# 112)def larg(number):
+#     listn = [3 ,123  ,124 ,12341]
+#     return max in listn
+# print(larg)
+
+
+# Write a function that returns the nth Fibonacci number.
+
+# 113)def larg(number):
+#     listn = [3 ,123  ,124 ,12341]
+#     return min in listn
+# print(larg)
+
+
+
+# Create a function that checks if a given string is a palindrome.
+
+# 114)def st(check):
+#     listn = [True,12,31,"Zuka"]
+#     for i in listn:
+#         if listn == str:
+#             return check
+#         print(st)
+
+# Write a function that takes a list and an element as arguments and returns the index of the 
+# 115)def name(take):
+#     listn = ["Apple" ,"Pennaple" ,"banana"]
+#     for i in listn:
+#         listn.pop(1)
+#         print(name)
+
+
+# element in the list.
+# 116)listn = ["Apple" ,"Pennaple" ,"banana"]
+# for i in listn:
+#     listn.pop(1)
+#     return listn
+
+
+# Create a function that takes a list of numbers and returns a new list with the squares of those numbers.
+
+# 117)def old(new):
+#     result = []
+#     for i in new:
+#         return result
+#     print(old)
+
+
+# Write a function that takes a string and returns the number of vowels in the string.
+
+# 118)string = ["Hello" ,"world"]
+# def stru(sit):
+#     result = []
+#     for i in string:
+#         print(str(string))
+#         return result
+#     print(stru,string)
+
+
+
+
+# 119)Create a function that merges two dictionaries.
+listn1 = ["First" ,"First"]
+listn2 = ["Second" ,"Second"]
+
+def name(name2):
+    result = []
+    for i in name2:
+        listn1+listn2
+    return result
+print(name)
+
+
+
+
+# Write a function that takes a variable number of arguments and returns their sum.
+
+# 120)def num1(num2):
+#     result = sum
+#     for i in num2:
+#         if num1 == num2:
+#             return result
+#         print(num2)
+
