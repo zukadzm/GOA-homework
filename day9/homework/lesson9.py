4386f81a4aba8e48dce5cb535843ad9bbb58a961
zukadzm <zuraqoridze988@gmail.com>
2024-07-27 07:12:05 -0700

    commit

--- day9/homework/lesson9.py
+++ day9/homework/lesson9.py
@@ -1,6 +1,32 @@
-#2) შეამომწმოთ იყოფა თუ არა მომხმარებლის მიერ შემოტანილი რიცხვი 2 ზე და 4ზე ზუსტად
-num = int(input("Enter your number: "))
+# #2) შეამომწმოთ იყოფა თუ არა მომხმარებლის მიერ შემოტანილი რიცხვი 2 ზე და 4ზე ზუსტად
+# num = int(input("Enter your number: "))
 
-for i in num:
-    num / 2
-    print(i)
+# for i in num:
+#     num / 2
+#     print(i)
+# #3) გააკეთოთ 10 მაგალითი შედარების ოპერატორებზე რომლებიც არის (>   <   >=     <=    ==   !=) 
+
+# # print(10 > 2)
+# # print(10 < 2)
+# # print(10 <= 0)
+# # print(10 => 0)
+# # print(10 == 0)
+# # print(10 != 0)
+# # print(10 > 2)
+# # print(10 < 2)
+# # print(10 <= 0)
+# # print(10 > 2)
+
+
+
+# #4) გააკეთეთ 10 მაგალითი and ოპერატორზე
+# print(0 and 0)
+# print(1 and 1)
+# print(10 and 2)
+# print(20 and 3)
+# print(30 and 4)
+# print(40 and 5)
+# print(50 and 6)
+# print(60 and 6)
+# print(70 and 7)
+# print(80 and 8)
