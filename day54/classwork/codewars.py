045366c475e40be662e2385146ea09d0f58643a2
zukadzm <zuraqoridze988@gmail.com>
2024-08-06 00:24:21 -0700

    commit

---
+++ day54/classwork/codewars.py
@@ -0,0 +1,7 @@
+# 1) https://www.codewars.com/kata/5208f99aee097e6552000148/train/python
+# 2) https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/python
+# 3) https://www.codewars.com/kata/5545f109004975ea66000086/train/python
+# 4) https://www.codewars.com/kata/54edbc7200b811e956000556/train/python
+# 5) https://www.codewars.com/kata/5951d30ce99cf2467e000013/train/python
+# 6) https://www.codewars.com/kata/567bf4f7ee34510f69000032/train/python
+# 7) https://www.codewars.com/kata/5417423f9e2e6c2f040002ae/train/python
