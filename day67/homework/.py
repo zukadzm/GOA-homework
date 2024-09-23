4fc149d5dbc1c140f319f29e978fdb8b58334cf3
zukadzm <zuraqoridze988@gmail.com>
2024-09-23 00:43:58 -0700

    commit

---
+++ day67/homework/.py
@@ -0,0 +1,6 @@
+# for i in range(0,100,2):
+#     print(i,"Odd")
+
+
+# for i in range(0,100,3):
+#     print(i,"Even")
---
+++ day69/homework/script.js
@@ -0,0 +1,76 @@
+// 1)Create an object: Make an object called car with properties like brand, model, and year.
+
+// const car = {
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: 2020
+// }
+
+// 2)Access object properties: Using the car object, access the brand and year properties and print them.
+
+// const car = {
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: 2020
+// }
+
+console.log(car.brand, car.model, car.year)
+
+
+
+// 3)Add a new property: Add a new property color to the car object.
+
+// const car = {
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: 2020
+// }
+
+// car.color = "Blue"
+// console.log(car)
+
+
+// 4)Change a property value: Update the year property of the car object to a new value.
+
+// const car = {
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: 2020
+// }
+
+// car.year = 2022,
+// console.log(car)
+
+
+// 5) Delete a property: Remove the model property from the car object.
+
+// const car = {
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: 2020
+// }
+
+// delete car.model;
+// console.log(car)
+
+
+// 6) Create a method: Add a method startEngine to the car object that prints "Engine started".
+
+// const car = {
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: 2020,
+//     startEngine: function() {
+//         console.log("Engine started")
+//     }
+// }
+
+
+// 7) Object inside an object: Create a new object owner inside the car object with properties like name and age.
+
+
+// const owner = {
+//     firstname: "John",
+//     age: 25
+// }
+// console.log(owner)
