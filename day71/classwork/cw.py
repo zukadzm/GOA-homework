24d4e6c515c4c259d8a80127fcebb27d3f4dc274
zukadzm <zuraqoridze988@gmail.com>
2024-09-25 02:19:22 -0700

    commit

---
+++ day71/classwork/cw.py
@@ -0,0 +1,21 @@
+# CODEWARS
+
+# 1) def get_sum(a, b):
+    # if a > b:
+    #     return get_sum(b, a)
+    # return sum(range(a, b+1))
+
+# 2) def number(lines):
+    # b = 1
+    # for i in range(len(lines)):
+    #     lines[i] = str(b) + ": " + lines[i]
+    #     b+=1
+    # return lines
+
+# 3_ def is_pangram(st):
+    # st = st.lower()
+    # if st == "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z":
+    #     return False
+    # else:
+    #     return True
+
---
+++ day71/homework/cw.py
@@ -0,0 +1,31 @@
+# CODEWARS
+# 1)def camel_case(s):
+    # s = s.split()
+    # result = ""
+    # for i in s:
+    #     i = i.capitalize()
+    #     result +=i
+    # return result
+
+# 2) def to_weird_case(string):
+    # i = 0
+    # ret = ''
+    # for char in string:
+    #     if char == ' ':
+    #         i = -1
+    #         ret = ret + char
+    #     elif (i % 2) == 0:
+    #         ret = ret + char.upper()
+    #     else:
+    #         ret = ret + char.lower()
+    #     i = i + 1
+    # return ret
+
+# 3) def clean_string(s):
+    # m = ''
+    # for i in s:
+    #     if i=='#':
+    #         m = m[:-1]
+    #     else:
+    #         m += i
+    # return m
