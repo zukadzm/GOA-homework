45b07910b76345b8e3e4bac7dadd1d7acbe74bf7
zukadzm <zuraqoridze988@gmail.com>
2024-10-01 07:02:09 -0700

    commit

---
+++ day73/homework/script.js
@@ -0,0 +1,107 @@
+// statement - განცხადება, კოდის ხაზი ერთი დავალება კომპიუტერისთვის
+// custom - კონრეტულად ჩვენთან მორგეული
+// კოდის ბლოკი - რაც უნდა შესრულდეს კონკრეტულ განხადებაში
+
+// 1.Multiply Two Numbers
+// Write a function multiply() that prompts the user to input two numbers and returns their product.
+
+// function multiply() {
+//     num1 = prompt("Enter your first number: ");
+//     num2 = prompt("Enter your second number: ");
+//     return num1 + num2;
+// }
+// multiply();
+
+
+// 2.Subtract Two Numbers
+// Write a function subtract() that prompts the user for two numbers and returns the difference.
+
+// function subtract() {
+//     num1 = prompt("Enter your first number: ");
+//     num2 = prompt("Enter your second number: ");
+//     return num1 - num2;
+// }
+// subtract();
+
+
+// 3.Divide Two Numbers
+// Write a function divide() that prompts the user for two numbers and returns their quotient.
+
+// function divide() {
+//     num1 = prompt("Enter your first number: ");
+//     num2 = prompt("Enter your second number: ");
+//     return num1 / num2;
+// }
+// divide();
+
+// 4.Return a Full Name
+// Write a function fullName() that prompts the user to input their first name and last name, and returns the full name as a single string.
+
+// function fullName() {
+//     firstName = prompt("Enter your first name: ");
+//     lastName = prompt("Enter your last name: ");
+//     return firstName + " " + lastName;
+// }
+// fullName();
+
+// 5.Convert Minutes to Seconds
+// Write a function minutesToSeconds() that prompts the user for a number of minutes and returns the equivalent in seconds.
+
+
+// function minutesToSeconds() {   
+//     minutes = promt("Enter a number of minutes");
+//     return minutes * 60;
+// }
+// minutesToSeconds();
+
+// 6.Calculate the Area of a Rectangle
+// Write a function rectangleArea() that prompts the user to input the length and width of a rectangle and returns the area.
+
+// function areaa() {
+//     length = prompt("Enter the length of the rectangle: ");
+//     width = prompt("Enter the width of the rectangle: ");
+//     return length * width;
+// }
+// areaa();
+
+// 7.Concatenate Two Strings
+// Write a function concatenateStrings() that prompts the user for two strings and returns them concatenated together.
+
+// function concatStrings() {
+//     str1 = prompt('Enter first string name or somthing: ');
+//     str2 = prompt('Enter second string name or something: ');
+//     return str1 + str2;
+// }
+// concatStrings();
+
+
+// 8.Exponentiation
+// Write a function power() that prompts the user for a base number and an exponent, and returns the result of raising the base to the power of the exponent.
+
+
+// function power() {
+//     number = prompt("Enter number: ");
+//     exponent = prompt("Enter exponent: ");
+//     return number + exponent;
+// }
+// power();
+
+// 9.Calculate Circle Circumference
+// Write a function circumference() that prompts the user to input a circle's radius and returns the circumference.
+
+
+// function circumference() {
+//     radius = prompt("Enter the radius of the circle: ");
+//     return radius * radius
+// }
+// circumference();
+
+
+// 10.Return the Next Number
+// Write a function nextNumber() that prompts the user for a number and returns the next number (the input number plus one).
+
+// function nextNumber() {
+//      num = promt("Enter your number:");
+//      return num + 1;
+// }
+// nextNumber();
