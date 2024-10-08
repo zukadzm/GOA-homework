80e3c3d2fed96fdab96c891e7bd202a89021cff6
zukadzm <zuraqoridze988@gmail.com>
2024-10-08 05:28:22 -0700

    commit

---
+++ day76/classwork/script.js
@@ -0,0 +1,65 @@
+// 1) შექმენით 20 წყვილი ცვლადი და შინახეთ მათ შიგნით true და false data types სხვადასხვა გზით და გამოიტანთ ეკრაზე მათი შედარება "და" და "ან" ლოგიკუ ოპერატორებით
+
+// let a = true;
+
+// let b = false;
+
+// let h = 5 > 19
+
+// console.log(h && b); // - False
+
+// console.log(a || h); // - True
+
+// console.log(10 || a); 
+
+// console.log(10 && 0); 
+
+// console.log(a > b);
+
+// console.log(a < b);
+
+// console.log(10 || h);
+
+// console.log(10 && b);
+
+// console.log(a || 10);
+
+// console.log(10 && 10);
+
+// console.log(100 && h);
+
+// console.log(100 || b);
+
+
+// 2) შექმენით ათი წყვილი ცვლადი და და თითოს მიანიჭეთ "მოსწორო" და "მოარასწორო" მნიშვენლობა. შემდეგ ლოგიკური ოპერატორების გამოყენებით გამოიტანეთ შესაბამისი მნიშვნელობა
+
+// let a = -0;
+
+// let b = "Hello";
+
+// let c = -0 > "Hello";
+
+// let d = 0 < undefined;
+
+// let e = "World" > 10;
+
+// let f = False < 7.6
+
+// let g = True > -0;
+
+// let h = True > False;
+
+// let i = True > 0;
+
+// let j = False > 0;
+
+// 3) გამოიყენეთ უკვე ნასწავლი მასალა და შეინახეთ ცვლადებში (სხვადასხვა გზით) true და false მნიშვნელობები. შემდეგ შეაბრუნეთ.
+
+// let i = ! false;
+
+// let j = ! true;
+
+// let a = ! a || b;
+
+
+// console.log(! j , j)
