aa569aac61d0fa906f4da6e604afe60226346b65
zukadzm <zuraqoridze988@gmail.com>
2024-10-09 01:34:49 -0700

    commit

---
+++ day77/classwork/script.js
@@ -0,0 +1,33 @@
+// // დავალება: მომხმარებელს შემოატანინეთ თავისი ასაკი, თუ მომხმარებელი იყო 18 წლზე პატარა დაუპრინტეთ შენ არასულწლოვანი ხარ, ხოლო სხვაშემთხვევაში შენ სულწლოვანი ხარ
+
+// const form = document.getElementById("myForm");
+// const button = document.getElementById("btn");
+
+// function checkAge() {
+//     let agee = form.elements.age;
+//     if(agee < 18) {
+//         console.log("You are small")
+//     }else {
+//         console.log("You are big")
+//     }
+// }
+
+// button.onclick = checkAge();
+
+// დავალება: for-ლუპის დახმარებით ჩაწერეთ ლისტში 1-დან 30-მდე რიცხვები, და შემდეგ შექმენით ახალი ლისტი სადაც ჩაწერთ იმ ლისტში მყოფ რიცხვებიდან მარტო ლუწ რიცხვებს
+
+// let list = []
+
+// for(let i = 0; i < 10; i++) {
+//     list.push(i);
+// }
+
+// let evennumber = []
+
+// for(let i = 0; list.length; i++) {
+//     if(list[i] % 2 === 0) {
+//         evennumber.push(list[i]);
+//     }
+// }
+
+// console(evennumber)
---
+++ day77/homework/script.js
@@ -0,0 +1,110 @@
+// Easy
+// დავალება NUM 1: შექმენი ფუნქცია, რომელიც მიიღებს რიცხვს და პირობით შეამოწმებს, არის თუ არა ის კენტი ან ლუწი.
+// დავალება NUM 2: შექმენი პირობა, რომელიც შეამოწმებს, არის თუ არა სია ცარიელი და თუ არა, იპოვოს რიცხვების ჯამი.
+// დავალება NUM 3: შექმენი ფუნქცია, რომელიც მიიღებს რიცხვების სიას, გაამრავლებს ყველა რიცხვს 3-ზე და დაიბრუნებს მხოლოდ იმ რიცხვებს, 
+// რომლებიც 20-ზე მეტია.
+
+// დავალება NUM 1: შექმენი ფუნქცია, რომელიც მიიღებს რიცხვს და პირობით შეამოწმებს, არის თუ არა ის კენტი ან ლუწი.
+// 1)let list = []
+
+// for(let i = 0; i < 30; i++) {
+//     list.push(i)
+// }
+
+// console.log(list);
+
+// function checkNUmber() {
+//     for(let i = 0; list.length; i++) {
+//         if(list[i] %2 === 0) {
+//             list.push(i)
+//         }
+//     }
+// }
+// checkNUmber();
+
+// დავალება NUM 2: შექმენი პირობა, რომელიც შეამოწმებს, არის თუ არა სია ცარიელი და თუ არა, იპოვოს რიცხვების ჯამი.
+
+// let arr = [1,2,3,45,,56];
+
+// function checkArray() {
+//     if(arr === Number) {
+//         return arr * arr
+//     } else if(!arr === number) {
+//         return console.log("fill bro fill")
+//     }
+// }
+// checkArray();
+
+// დავალება NUM 3: შექმენი ფუნქცია, რომელიც მიიღებს რიცხვების სიას, გაამრავლებს ყველა რიცხვს 3-ზე და დაიბრუნებს მხოლოდ იმ რიცხვებს, 
+// რომლებიც 20-ზე მეტია.
+
+// let list = [1,2,3,4,5,6,7,89,8,10];
+
+// function checkList() {
+//     return list * 3
+// }
+// checkList();
+
+
+// 
+// Medium
+// დავალება NUM 4: შექმენი პირობა, რომელიც შეამოწმებს სიაში უდიდეს რიცხვს და დააბრუნებს მას.
+// დავალება NUM 5: დაწერე ფუნქცია, რომელიც მიიღებს ორ რიცხვს და პირობით შეამოწმებს, თუ მათი ნამრავლი 100-ზე მეტია.
+
+// let listNumber = [1,2,3,4,5,6,132,56,6321];
+
+// function findMax() {
+//     let max = listNumber[0]
+//     for(let i = 0; listNumber.length; i++) {
+//         if(listNumber[i] > listNumber) {
+//             return alert("Find max")
+//         }
+//     }
+// }
+// listNumber();
+
+// let listNumber = [1,2,3,4,5];
+
+// function num() {
+//     if(listNumber *  100) {
+//         return listNumber * 100
+//     }
+// }
+// listNumber();
+
+// Chad
+// დავალება NUM 6: შექმენი ფუნქცია, რომელიც მიიღებს ტექსტს და შეამოწმებს, თუ ტექსტის პირველ და ბოლო ასოები ერთნაირია.
+// დავალება NUM 7: შექმენი ფუნქცია, რომელიც მიიღებს ტექსტს და დააბრუნებს თითოეული ასოს სიხშირეს, ანუ რამდენჯერ გვხვდება თითოეული ასო ტექსტში.
+
+// let text = "Hello world";
+
+// function checkText() {
+//     if(text[0] === "Hello" || "world") {
+//         return console.log("First and last letters are the same")
+//     }
+// }
+// checkText()
+
+// let text = "Hello world";
+
+// function checkText() {
+//     if(text === "l" || "l") {
+//         return "l"
+//     }
+// }
+// text();
+
+// Giga chad
+// დავალება NUM 8: შექმენი ფუნქცია, რომელიც მიიღებს ტექსტს და შეამოწმებს, არის თუ არა ის პალინდრომი (ტექსტი, რომელიც წაკითხვისას ერთნაირად ჩანს როგორც წინ და უკან, მაგალითად: "level").
+
+// let txt = "level";
+
+// function checkText() {
+//     let reverse_text = "level";
+//     if(txt === reverse_text) {
+//         return console.log("Text is palindrome");
+//     }
+    
+// }
+
+// checkText();
