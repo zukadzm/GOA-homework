ec13963d7b37da99a68e785849431fecb2633b9d
zukadzm <zuraqoridze988@gmail.com>
2024-11-05 00:06:28 -0800

    commit

---
+++ day88/classwork/cw.py
@@ -0,0 +1,17 @@
+# // გამოვიყენოთ ეს ფუნქციები, შევინახოთ მნიშვნელობა და გამოვპრიტოთ ის
+
+def add(a,b):
+    return a+b
+
+def gamokleba(a,b):
+    return a-b
+
+arg1 = 7
+arg2 = 14
+arg3 = 3
+
+
+sumnmb = add(arg1 + arg2)
+sumnmb2 = gamokleba(arg2 - arg2)
+print(sumnmb)
+print(sumnmb2)
