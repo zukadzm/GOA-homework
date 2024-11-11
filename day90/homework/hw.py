12425ffadd7f1d0c92125282fa5031df81e0f9ab
zukadzm <zuraqoridze988@gmail.com>
2024-11-11 04:28:04 -0800

    commit

---
+++ day90/homework/hw.py
@@ -0,0 +1,9 @@
+# დაწერეთ count_area ფუნქცია რომელსაც შეეძლება გამოთვალოს ფიგურის ფართობი მართკუთხედისთვის(ინფუთი იქნება 2  რიცხვი) ,  წრისთვის (ინფუთი იქნება 1 მთელი რიცხვი ) და სამკუთხედისთვის (ინფუთი იქნება 3 მთელი რიცხვი)
+# ( პითონში და ასევე ჯავასკიპტში ) 
+
+user_input_first = int(input('Enter first number: '))
+user_input_second = int(input('Enter second number: '))
+
+def count_area(a, b):
+    return a + b
+print(count_area(user_input_first,user_input_second))
---
+++ day90/homework/script.js
@@ -0,0 +1,10 @@
+// დაწერეთ count_area ფუნქცია რომელსაც შეეძლება გამოთვალოს ფიგურის ფართობი მართკუთხედისთვის(ინფუთი იქნება 2  რიცხვი) ,  წრისთვის (ინფუთი იქნება 1 მთელი რიცხვი ) და სამკუთხედისთვის (ინფუთი იქნება 3 მთელი რიცხვი)
+// ( პითონში და ასევე ჯავასკიპტში ) 
+
+const firstinput = Number(prompt('Enter your first number'));
+const secondinput = Number(prompt('Enter your second number'));
+
+function count_area(a, b){
+    return a + b
+}
+console.log(count_area(firstinput,secondinput));
