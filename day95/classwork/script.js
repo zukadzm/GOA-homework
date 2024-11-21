e1cde2c2b0c56060638063141b48c02431c7ca53
zukadzm <zuraqoridze988@gmail.com>
2024-11-20 23:51:08 -0800

    commit

---
+++ day95/classwork/script.js
@@ -0,0 +1,13 @@
+const first = document.getElementById("firstt");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+
+let all = [first,second,third]
+
+for(let i = 1; 1 > all.length; i++); {
+    const all = {
+        first: "კომპიტერი",
+        second: "მაუსი",
+        third: "კლავიატურა"
+    }
+}
