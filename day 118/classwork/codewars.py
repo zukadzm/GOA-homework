36fc4226d5d9fbc164fe04b0bdde9e4f7ed90b94
zukadzm <zuraqoridze988@gmail.com>
2025-01-14 01:32:28 -0800

    commit

---
+++ day 118/classwork/codewars.py
@@ -0,0 +1,10 @@
+# def calculator(string):
+#     [x, op, y] = string.split(' ')
+#     if op == '+':
+#         return '.' * (len(x) + len(y))
+#     elif op == '*':
+#         return '.' * (len(x) * len(y))
+#     elif op == '-':
+#         return '.' * (len(x) - len(y))
+#     else:
+#         return '.' * int(len(x) / len(y))
