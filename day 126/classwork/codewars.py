95ced4697041f58977db6d55e658daca79b30083
zukadzm <zuraqoridze988@gmail.com>
2025-02-02 01:12:15 -0800

    commit

---
+++ day 126/classwork/codewars.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/python
+
+# def sum_array(arr):
+#     if arr and len(arr)>2:
+#         arr.remove(max(arr))
+#         arr.remove(min(arr))
+#         return sum(arr)
+#     else:
+#         return 0
