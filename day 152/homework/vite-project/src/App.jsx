b907374705a72ec60b12a43ec97968625fba9a4c
zukadzm <zuraqoridze988@gmail.com>
2025-04-12 17:03:51 +0300

    commit

--- day 152/homework/vite-project/src/App.jsx
+++ day 152/homework/vite-project/src/App.jsx
@@ -1,19 +1,3 @@
-// import Home from './Home';
-// import About from './About';
-// import Contact from './Contact';
-
-// function App() {
-//   return (
-//     <div>
-//       <Home />
-//       <About />
-//       <Contact />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import Home from "./home.jsx";
 import About from "./about.jsx";
 import Contact from "./contact.jsx";
